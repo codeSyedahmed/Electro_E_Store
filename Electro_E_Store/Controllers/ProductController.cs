af958930df9abcf075f445e86818112e97e552d8
syedahmed <code.syedahmed@gmail.com>
2024-09-26 22:58:12 +0500

    Product add done

--- Electro_E_Store/Controllers/ProductController.cs
+++ Electro_E_Store/Controllers/ProductController.cs
@@ -1,6 +1,7 @@
 ﻿using Electro_E_Store.Models;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -15,10 +16,142 @@ namespace Electro_E_Store.Controllers
         {
             return View();
         }
-
+        [HttpGet]
         public ActionResult AddProduct()
         {
+            ViewBag.Categories = new SelectList(db.tb_Categories, "category_id", "category_name");
             return View();
         }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult AddProduct(tb_Products products, HttpPostedFileBase fileBase)
+        {
+            ViewBag.Categories = new SelectList(db.tb_Categories, "category_id", "category_name");
+            if (ModelState.IsValid)
+			{
+    //            var productExists = db.tb_Products.Any(x => x.product_name.Equals(products.product_name));
+				//if (productExists)
+				//{
+    //                ModelState.AddModelError("product_name", "Product name already exists");
+    //                return View(products);
+    //            }
+    //            var modelExists = db.tb_Products.Any(x => x.model.Equals(products.model));
+    //            if (modelExists)
+    //            {
+    //                ModelState.AddModelError("model", "Model number already registered");
+    //                return View(products);
+    //            }
+
+                Dictionary<string, string> fileUpload = UploadImage(fileBase);
+				if (fileUpload.ContainsKey("success"))
+				{
+                    products.product_img = fileUpload["success"];
+                    products.inserted_at = DateTime.Now;
+                    products.updated_at = DateTime.Now;
+                    db.tb_Products.Add(products);
+                    db.SaveChanges();
+                    TempData["success"] = "Product added successfully!";
+                    return RedirectToAction("ProductList");
+                }
+				else
+				{
+                    TempData["error"] = fileUpload["error"];
+                    return View(products);
+                }
+
+            }
+            return View(products);
+        }
+
+        //      private Dictionary<string, string> UploadImage(HttpPostedFileBase imageFile, string productFileName = "")
+        //{
+        //          Dictionary<string, string> result = new Dictionary<string, string>();
+
+        //}
+
+        private bool DeleteImage(string filename)
+        {
+            bool result = false;
+            if (fileALreadyExist(filename))
+            {
+                try
+                {
+                    string path = Server.MapPath(filename);
+                    FileInfo file = new FileInfo(path);
+                    file.Delete();
+                    result = true;
+                }
+                catch
+                {
+                    result = false;
+                }
+            }
+            else
+            {
+                result = false;
+            }
+            return result;
+        }
+        private bool fileALreadyExist(string filename)
+        {
+            string path = Server.MapPath(filename);
+            FileInfo file = new FileInfo(path);
+            if (file.Exists)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        private Dictionary<string, string> UploadImage(HttpPostedFileBase file, string productFileName = "")
+		{
+            Dictionary<string, string> result = new Dictionary<string, string>();
+            if (file != null && file.ContentLength > 0)
+			{
+                string filename = Path.GetFileNameWithoutExtension(file.FileName);
+                filename = "~/UploadImages/" + filename;
+
+				if (fileALreadyExist(filename))
+				{
+                    result.Add("error", "Another file with same name already exists, Choose different or rename your file!");
+                    return result;
+				}
+
+                string extension = Path.GetExtension(file.FileName);
+
+                if(extension.ToLower().Equals(".jpg")|| extension.ToLower().Equals(".jpeg") || extension.ToLower().Equals(".png"))
+				{
+					try
+					{
+                        filename += extension;
+                        string path = Server.MapPath(filename);
+                        file.SaveAs(path);
+
+						if (productFileName != "")
+						{
+							DeleteImage(productFileName);
+						}
+
+						result.Add("success", filename);
+					}
+					catch
+					{
+                        result.Add("error", "Some unknown error occured while uploading the file!");
+                    }
+				}
+				else
+				{
+                    result.Add("error", "Only jpg, jpeg and png formats are allowed!");
+                }
+            }
+			else
+			{
+                result.Add("error", "Please select a file");
+            }
+            return result;
+		}
     }
 }
